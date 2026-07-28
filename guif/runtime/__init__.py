533668ecd843fba439e12286ebee713df77a3a78
Eason <58758714+943065241@users.noreply.github.com>
2026-07-28 19:40:36 +0800

    feat(runtime): export persistence APIs

--- guif/runtime/__init__.py
+++ guif/runtime/__init__.py
@@ -1,7 +1,8 @@
 from guif.runtime.context import RuntimeContext, load_runtime_context
 from guif.runtime.pipeline import Pipeline
 from guif.runtime.registry import AgentRegistry
-from guif.runtime.runtime import Runtime
+from guif.runtime.runtime import Runtime, RuntimeExecutionError
+from guif.runtime.store import TaskStore
 from guif.runtime.task import Task, TaskEvent
 
 __all__ = [
@@ -9,7 +10,9 @@ __all__ = [
     "Pipeline",
     "Runtime",
     "RuntimeContext",
+    "RuntimeExecutionError",
     "Task",
     "TaskEvent",
+    "TaskStore",
     "load_runtime_context",
 ]
