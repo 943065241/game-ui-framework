1624c93e425bd57ea45884cfb23028639fbd2196
Eason <58758714+943065241@users.noreply.github.com>
2026-07-29 11:49:28 +0800

    feat: export provider execution error

--- guif/runtime/__init__.py
+++ guif/runtime/__init__.py
@@ -1,13 +1,14 @@
 from guif.runtime.context import RuntimeContext, load_runtime_context
 from guif.runtime.pipeline import Pipeline
 from guif.runtime.registry import AgentRegistry
-from guif.runtime.runtime import Runtime, RuntimeExecutionError
+from guif.runtime.runtime import ProviderExecutionError, Runtime, RuntimeExecutionError
 from guif.runtime.store import TaskStore
 from guif.runtime.task import Task, TaskEvent
 
 __all__ = [
     "AgentRegistry",
     "Pipeline",
+    "ProviderExecutionError",
     "Runtime",
     "RuntimeContext",
     "RuntimeExecutionError",
