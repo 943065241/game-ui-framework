78c3fb9a5d6395b0ba29bd0f87e7941e04dcb771
Eason <58758714+943065241@users.noreply.github.com>
2026-07-29 16:28:36 +0800

    feat: export operational Runtime

--- guif/runtime/__init__.py
+++ guif/runtime/__init__.py
@@ -1,7 +1,12 @@
+from guif.auth import AuthenticatedActor, AuthenticationError
+from guif.concurrency import ConcurrencyError, LeaseError
 from guif.gated_export import GatedExportError
+from guif.git_changes import GitChangeError
+from guif.host_api import HostCallbackError
 from guif.runtime.context import RuntimeContext, load_runtime_context
+from guif.runtime.operational import Runtime
 from guif.runtime.pipeline import Pipeline
-from guif.runtime.private_theme import Runtime, ThemeResolutionRequired
+from guif.runtime.private_theme import ThemeResolutionRequired
 from guif.runtime.registry import AgentRegistry
 from guif.runtime.runtime import ProviderExecutionError, RuntimeExecutionError
 from guif.runtime.store import TaskStore
@@ -10,7 +15,13 @@ from guif.tool_execution import ToolExecutionError
 
 __all__ = [
     "AgentRegistry",
+    "AuthenticatedActor",
+    "AuthenticationError",
+    "ConcurrencyError",
     "GatedExportError",
+    "GitChangeError",
+    "HostCallbackError",
+    "LeaseError",
     "Pipeline",
     "ProviderExecutionError",
     "Runtime",
