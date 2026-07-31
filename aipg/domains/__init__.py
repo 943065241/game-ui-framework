fc406d3e303913fe1c292c462c77b9da4f64057c
Eason <58758714+943065241@users.noreply.github.com>
2026-07-31 15:10:27 +0800

    导出 AIPG Domain Registry

--- aipg/domains/__init__.py
+++ aipg/domains/__init__.py
@@ -1,3 +1,21 @@
+from aipg.domains.governance import FRAMEWORK_GOVERNANCE_DOMAIN
+from aipg.domains.model import DomainPackDefinition
+from aipg.domains.registry import (
+    BUILTIN_DOMAIN_REGISTRY,
+    DomainRegistry,
+    domain_for_workflow,
+    get_domain_pack,
+    list_domain_packs,
+)
 from aipg.domains.visual import GUIF_VISUAL_DOMAIN
 
-__all__ = ["GUIF_VISUAL_DOMAIN"]
+__all__ = [
+    "BUILTIN_DOMAIN_REGISTRY",
+    "DomainPackDefinition",
+    "DomainRegistry",
+    "FRAMEWORK_GOVERNANCE_DOMAIN",
+    "GUIF_VISUAL_DOMAIN",
+    "domain_for_workflow",
+    "get_domain_pack",
+    "list_domain_packs",
+]
